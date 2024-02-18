c5e5905cf97fe070505b8aa2b4a76fef6442c431
redouane <redouane.hitachi@gmail.com>
2024-02-18 04:02:43 -0800

    tapping anglar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { faker } from '@faker-js/faker';
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'tappingChallange';
+  randomText = faker.lorem.sentence();
+  enteredText = '';
+  onInput(value: string) {
+    this.enteredText = value;
+  }
+  Compare(letter:string,enterdtext:string){
+    if (!enterdtext) {
+      return "pending";
+    }
+   
+    return letter===enterdtext?"correct":"incorrect"
+  }
 }
